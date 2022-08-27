63b475dfaa07afe03ef00d6ffcd526aa3267fe8f
Juhyung990122 <harukawa99@naver.com>
2022-08-27 15:49:24 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/yoeleum/keyduck/KeyduckApplication.java
@@ -0,0 +1,13 @@
+package com.yoeleum.keyduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyduckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyduckApplication.class, args);
+    }
+
+}
